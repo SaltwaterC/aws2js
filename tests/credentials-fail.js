6b39774afd38a9b54fc95b360d32e385e6f2d4a9
Stefan Rusu <saltwaterc@gmail.com>
2011-09-06 12:30:12 +0300

    Includes a callback test.

--- tests/credentials-fail.js
+++ tests/credentials-fail.js
@@ -1,7 +1,14 @@
 var ec2 = require('../').load('ec2');
 var assert = require('assert');
 
+var callback = false;
+
 ec2.call('DescribeInstances', {}, function (err, res) {
+	callback = true;
 	assert.ok(err instanceof Error);
 	assert.deepEqual(err.message, 'You must set the AWS credentials: accessKeyId + secretAccessKey');
+});
+
+process.on('exit', function () {
+	assert.ok(callback);
 });
