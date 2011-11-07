1e8de6f741d91d88096dfd1963dd5bf57772cbd9
Stefan Rusu <saltwaterc@gmail.com>
2011-11-07 16:40:08 +0200

    CloudWatch test.

---
+++ tests/cloudwatch.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var cloudwatch = require('../').load('cloudwatch');
+
+cloudwatch.setCredentials(process.env.AWS_ACCEESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
+cloudwatch.setRegion('us-east-1');
+
+var callbacks = {
+	request: false,
+	requestWithoutQuery: false
+};
+
+var cloudwatchProcessResponse = function (err, res) {
+	assert.ifError(err);
+	assert.ok(res.DescribeAlarmsResult.MetricAlarms);
+};
+
+cloudwatch.request('DescribeAlarms', {}, function (err, res) {
+	callbacks.request = true;
+	cloudwatchProcessResponse(err, res);
+});
+
+cloudwatch.request('DescribeAlarms', function (err, res) {
+	callbacks.requestWithoutQuery = true;
+	cloudwatchProcessResponse(err, res);
+});
+
+process.on('exit', function () {
+	for (var i in callbacks) {
+		assert.ok(callbacks[i]);
+	}
+});
