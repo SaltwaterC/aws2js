88e0a52d7f42eaa68c3a844c9ce4f56617dea15b
Stefan Rusu <saltwaterc@gmail.com>
2011-11-07 16:05:29 +0200

    SES test.

---
+++ tests/ses.js
@@ -0,0 +1,37 @@
+var assert = require('assert');
+
+var ses = require('../').load('ses');
+
+ses.setCredentials(process.env.AWS_ACCEESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
+
+try {
+	rds.setRegion('us-east-1');
+} catch (e) {
+	assert.ok(e instanceof Error);
+}
+
+var callbacks = {
+	request: false,
+	requestWithoutQuery: false
+};
+
+var sesProcessResponse = function (err, res) {
+	assert.ifError(err);
+	assert.ok(res.ListVerifiedEmailAddressesResult.VerifiedEmailAddresses);
+};
+
+ses.request('ListVerifiedEmailAddresses', {}, function (err, res) {
+	callbacks.request = true;
+	sesProcessResponse(err, res);
+});
+
+ses.request('ListVerifiedEmailAddresses', function (err, res) {
+	callbacks.requestWithoutQuery = true;
+	sesProcessResponse(err, res);
+});
+
+process.on('exit', function () {
+	for (var i in callbacks) {
+		assert.ok(callbacks[i]);
+	}
+});
