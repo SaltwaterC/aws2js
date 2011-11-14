92ad18a4f0b6cb752e5d37a4d102326348558738
Stefan Rusu <saltwaterc@gmail.com>
2011-11-14 16:04:10 +0200

    S3 various object tests.

--- tests/s3-pdf.js
+++ tests/s3-pdf.js
@@ -1,24 +1,23 @@
 var assert = require('assert');
 var s3 = require('../').load('s3');
-var path = 'foo~!@#$&*()=:,;?+\'.txt';
+var path = 'foo.pdf';
 
 var callbacks = {
 	put: false,
-	get: false,
+	head: false,
 	del: false
 };
 
 s3.setCredentials(process.env.AWS_ACCEESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
 s3.setBucket(process.env.AWS2JS_S3_BUCKET);
 
-s3.putObject(path, './data/foo.txt', false, {}, function (err, res) {
+s3.putObject(path, './data/foo.pdf', false, {}, function (err, res) {
 	callbacks.put = true;
 	assert.ifError(err);
-	s3.get(path, 'buffer', function (err, res) {
-		callbacks.get = true;
+	s3.head(path, function (err, res) {
+		callbacks.head = true;
 		assert.ifError(err);
-		assert.deepEqual(res.headers['content-type'], 'text/plain');
-		assert.deepEqual(res.buffer, 'bar\n');
+		assert.deepEqual(res['content-type'], 'application/pdf');
 		s3.del(path, function (err) {
 			callbacks.del = true;
 			assert.ifError(err);
