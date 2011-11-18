27a03c3157f99a589b03756f8e71c97a3cf5d97b
Stefan Rusu <saltwaterc@gmail.com>
2011-11-18 10:36:02 +0200

    Meaner checkConfig() internal. If the credentials aren't set, then it throws an Error().

--- tests/credentials-fail.js
+++ tests/credentials-fail.js
@@ -1,14 +1,9 @@
 var ec2 = require('../').load('ec2');
 var assert = require('assert');
 
-var callback = false;
-
-ec2.request('DescribeInstances', {}, function (err, res) {
-	callback = true;
+try {
+	ec2.request('DescribeInstances');
+} catch (err) {
 	assert.ok(err instanceof Error);
 	assert.deepEqual(err.message, 'You must set the AWS credentials: accessKeyId + secretAccessKey');
-});
-
-process.on('exit', function () {
-	assert.ok(callback);
-});
+}
