f6c14b14f6b0a20b5828c31828c32241ae22672e
Stefan Rusu <saltwaterc@gmail.com>
2011-12-08 10:07:52 +0200

    AWS CloudFormation support.

---
+++ tests/cloudformation.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+
+var cloudformation = require('../').load('cloudformation');
+
+cloudformation.setCredentials(process.env.AWS_ACCEESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
+cloudformation.setRegion('us-east-1');
+
+var callbacks = {
+	request: false,
+	requestWithoutQuery: false
+};
+
+var cloudformationProcessResponse = function (err, res) {
+	assert.ifError(err);
+	assert.ok(res.DescribeStacksResult.Stacks);
+};
+
+cloudformation.request('DescribeStacks', {}, function (err, res) {
+	callbacks.request = true;
+	cloudformationProcessResponse(err, res);
+});
+
+cloudformation.request('DescribeStacks', function (err, res) {
+	callbacks.requestWithoutQuery = true;
+	cloudformationProcessResponse(err, res);
+});
+
+process.on('exit', function () {
+	for (var i in callbacks) {
+		assert.ok(callbacks[i]);
+	}
+});
