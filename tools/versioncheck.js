13356588a0adbec110248c702f6a5b027903c559
Stefan Rusu <saltwaterc@gmail.com>
2012-03-23 12:36:46 +0200

    Changes the STS doc URL due to Amazon changing the structure.

--- tools/versioncheck.js
+++ tools/versioncheck.js
@@ -16,7 +16,7 @@ var docs = {
 	sqs: 'http://docs.amazonwebservices.com/AWSSimpleQueueService/latest/APIReference/Welcome.html',
 	cloudformation: 'http://docs.amazonwebservices.com/AWSCloudFormation/latest/APIReference/Welcome.html',
 	sdb: 'http://docs.amazonwebservices.com/AmazonSimpleDB/latest/DeveloperGuide/Welcome.html',
-	sts: 'http://docs.amazonwebservices.com/STS/latest/APIReference/_title.html',
+	sts: 'http://docs.amazonwebservices.com/STS/latest/APIReference/Welcome.html',
 	dynamodb: 'http://docs.amazonwebservices.com/amazondynamodb/latest/developerguide/Introduction.html',
 	sns: 'http://docs.amazonwebservices.com/sns/latest/api/Welcome.html'
 };
@@ -32,9 +32,13 @@ var check = function (service, url, current) {
 			if (version && version[1]) {
 				if (version[1] != current) {
 					console.log('%s has a newer version: %s', service, version[1]);
+				} else {
+					if (process.env.debug) {
+						console.error('%s has the same version: %s', service, version[1]);
+					}
 				}
 			} else {
-				console.log('Did not find a version for: %s', service);
+				console.error('Did not find a version for: %s', service);
 			}
 		}
 	});
