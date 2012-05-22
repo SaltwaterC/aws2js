366ee975bddd237e8133cb93c443e6aefcbc8e7a
Stefan Rusu <saltwaterc@gmail.com>
2012-05-22 11:26:37 +0300

    Bunch of refactoring. Now with bigger screen included.

--- tests/s3-lifecycle.js
+++ tests/s3-lifecycle.js
@@ -2,10 +2,10 @@ var assert = require('assert');
 var s3 = require('../').load('s3');
 
 var callbacks = {
-    putLifeCycleRule1: false,
-    putLifeCycleRule2: false,
-    delLifeCycleRule1: false,
-    delLifeCycleRule2: false
+	putLifeCycleRule1: false,
+	putLifeCycleRule2: false,
+	delLifeCycleRule1: false,
+	delLifeCycleRule2: false
 };
 
 s3.setCredentials(process.env.AWS_ACCEESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
@@ -14,15 +14,15 @@ s3.setBucket(process.env.AWS2JS_S3_BUCKET);
 s3.putLifeCycleRule('id', 'prefix', 5, function (error, response) {
 	callbacks.putLifeCycleRule1 = true;
 	assert.ifError(error);
-
+	
     s3.putLifeCycleRule('id2', 'otherprefix', 5, function (error, response) {
         callbacks.putLifeCycleRule2 = true;
         assert.ifError(error);
-
+		
         s3.delLifeCycleRule('id', function(error, response) {
             callbacks.delLifeCycleRule1 = true;
             assert.ifError(error);
-
+			
             s3.delLifeCycleRule('id2', function(error, response) {
                 callbacks.delLifeCycleRule2 = true;
                 assert.ifError(error);
