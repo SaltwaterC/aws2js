9f05c83f63585970efd88ac03da70c77d25d3e8b
Stefan Rusu <saltwaterc@gmail.com>
2012-08-30 19:13:58 +0300

    Improved the test stability that uses to crash often due to the non-transactional nature of S3.

--- tests/s3-lifecycle.js
+++ tests/s3-lifecycle.js
@@ -10,25 +10,53 @@ var callbacks = {
 	delLifeCycleRule2: false
 };
 
+var timestamp = new Date().getTime();
+
+var showError = function (err) {
+	if (err) {
+		console.error(err);
+	}
+};
+
 s3.setCredentials(process.env.AWS_ACCEESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
 s3.setBucket(process.env.AWS2JS_S3_BUCKET);
 
-s3.putLifeCycleRule('id', 'prefix', 5, function (error, response) {
-	callbacks.putLifeCycleRule1 = true;
+s3.delLifeCycle(function (error, response) {
+	console.log('s3.delLifeCycle');
+	showError(error);
 	assert.ifError(error);
 	
-	s3.putLifeCycleRule('id2', 'otherprefix', 5, function (error, response) {
-		callbacks.putLifeCycleRule2 = true;
+	s3.putLifeCycleRule('id', 'prefix-' + timestamp, 5, function (error, response) {
+		callbacks.putLifeCycleRule1 = true;
+		
+		console.log('s3.putLifeCycleRule: id, prefix-' + timestamp);
+		showError(error);
 		assert.ifError(error);
 		
-		s3.delLifeCycleRule('id', function(error, response) {
-			callbacks.delLifeCycleRule1 = true;
+		s3.putLifeCycleRule('id2', 'otherprefix-' + timestamp, 5, function (error, response) {
+			callbacks.putLifeCycleRule2 = true;
+			
+			console.log('s3.putLifeCycleRule: id2, otherprefix-' + timestamp);
+			showError(error);
 			assert.ifError(error);
 			
-			s3.delLifeCycleRule('id2', function(error, response) {
-				callbacks.delLifeCycleRule2 = true;
-				assert.ifError(error);
-			});
+			setTimeout(function () {
+				s3.delLifeCycleRule('id', function(error, response) {
+					callbacks.delLifeCycleRule1 = true;
+					
+					console.log('s3.delLifeCycleRule: id');
+					showError(error);
+					assert.ifError(error);
+					
+					s3.delLifeCycleRule('id2', function(error, response) {
+						callbacks.delLifeCycleRule2 = true;
+						
+						console.log('s3.delLifeCycleRule: id2');
+						showError(error);
+						assert.ifError(error);
+					});
+				});
+			}, 10000);
 		});
 	});
 });
