ede337e02a5d37fcdf0ddd18bc3fa6acfcbc2582
Ștefan Rusu <saltwaterc@gmail.com>
2012-12-18 11:44:06 +0200

    Makes sure the script is portable to OS X with its retarded version of dd and lack of md5sum.

--- tools/createtemp.sh
+++ tools/createtemp.sh
@@ -1,4 +1,27 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-dd if=/dev/urandom of=6M.tmp bs=1M count=6 2>/dev/null
-md5sum 6M.tmp | cut -d' ' -f1
+function echoe()
+{
+	echo "$@" 1>&2
+}
+
+dd if=/dev/urandom of=6M.tmp bs=1048576 count=6 2>/dev/null
+
+EXITCODE=$?
+
+if [ $EXITCODE -ne 0 ]
+then
+	echoe "ERROR: dd failed to create the temp file"
+	exit $EXITCODE
+fi
+
+which md5sum 1>/dev/null 2>&1
+
+if [ $? -eq 0 ]
+then
+	md5sum=md5sum
+else
+	md5sum=gmd5sum
+fi
+
+$md5sum 6M.tmp | cut -d' ' -f1
