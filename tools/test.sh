8b87ca7931b3e8800adb7c244a39ee29ea3d4280
Ștefan Rusu <saltwaterc@gmail.com>
2013-01-10 18:08:17 +0200

    Brings some colors in the life.

--- tools/test.sh
+++ tools/test.sh
@@ -20,32 +20,41 @@ for TEST in $(ls ./*.js)
 do
 	if [ -f $TEST ]
 	then
-		TEST_FILE=$(basename $TEST)
+		TEST_FILE=$(basename $TEST .js)
 		PERCENT=$(echo "$CURRENT / $TOTAL * 100" | bc -l | awk '{printf("%d\n",$1 + 0.5)}')
 		OUTPUT="\r[$PERCENT% | $CURRENT/$TOTAL | + $SUCCESS | - $FAIL] $TEST_FILE"
 		echo -ne $OUTPUT
 		$NODE_BIN $TEST > /dev/null
 		EXIT_CODE=$?
-		if [ $EXIT_CODE -ne 0 ]
-		then
-			echo -e "\nFailed: $TEST_FILE"
-			FAIL=$(($FAIL + 1))
-		else
-			SUCCESS=$(($SUCCESS+1))
-		fi
+
 		SPACER=""
 		for IDX in $(seq 0 ${#OUTPUT})
 		do
 			SPACER=" $SPACER"
 		done
 		echo -ne "\r$SPACER"
+
+		if [ $EXIT_CODE -ne 0 ]
+		then
+			echo -e "\n\e[0;31mFailed: $TEST_FILE\e[0m\n"
+			FAIL=$(($FAIL + 1))
+		else
+			SUCCESS=$(($SUCCESS+1))
+		fi
 		CURRENT=$(($CURRENT + 1))
 	fi
 done
 
 echo ""
-echo "Failed tests: $FAIL"
-echo "Total tests: $TOTAL"
+if [ $FAIL -eq 0 ]
+then
+	# green
+	echo -e "\e[0;32mFailed tests: $FAIL\e[0m"
+else
+	#red
+	echo -e "\e[0;31mFailed tests: $FAIL\e[0m"
+fi
+echo -e "\033[1mTotal tests: $TOTAL\033[0m"
 echo ""
 
 if [ $FAIL -eq 0 ]
