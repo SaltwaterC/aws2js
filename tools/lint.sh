994b196c91d78d7be5bda17bd5064d4ae5f45753
Ștefan Rusu <saltwaterc@gmail.com>
2013-01-21 13:38:58 +0200

    Makes sure jslint is always installed.

--- tools/lint.sh
+++ tools/lint.sh
@@ -2,10 +2,14 @@
 
 function lint
 {
+	jslint=$(which jslint)
+	if [ -z "$jslint" ]
+	then
+		npm -g install jslint
+	fi
 	output=$(find $1 -name "*.js" -print0 | xargs -0 jslint --plusplus --white --var --goodparts --node | grep -v "is OK." | grep '[^[:space:]]')
 	exit=$?
 	echo "$output"
-	
 	if [ $exit -eq 0 ]
 	then
 		exit 1
