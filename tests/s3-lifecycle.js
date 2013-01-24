92ebd37c3ecde66faf3f4947b365138541752e81
Ștefan Rusu <saltwaterc@gmail.com>
2013-01-24 17:26:15 +0200

    Improved the stability of the s3-lifecycle test.

--- tests/s3-lifecycle.js
+++ tests/s3-lifecycle.js
@@ -6,10 +6,9 @@ var assert = require('assert');
 var s3 = require('../').load('s3');
 
 var callbacks = {
-	putLifeCycleRule1: 0,
-	putLifeCycleRule2: 0,
-	delLifeCycleRule1: 0,
-	delLifeCycleRule2: 0
+	delLifeCycle: 0,
+	putLifeCycleRule: 0,
+	delLifeCycleRule: 0
 };
 
 var timestamp = new Date().getTime();
@@ -24,41 +23,25 @@ s3.setCredentials(process.env.AWS_ACCEESS_KEY_ID, process.env.AWS_SECRET_ACCESS_
 s3.setBucket(process.env.AWS2JS_S3_BUCKET);
 
 s3.delLifeCycle(function (error, response) {
+	callbacks.delLifeCycle++;
+	
 	console.log('s3.delLifeCycle');
 	showError(error);
 	assert.ifError(error);
 	
 	s3.putLifeCycleRule('id', 'prefix-' + timestamp, 5, function (error, response) {
-		callbacks.putLifeCycleRule1++;
+		callbacks.putLifeCycleRule++;
 		
 		console.log('s3.putLifeCycleRule: id, prefix-' + timestamp);
 		showError(error);
 		assert.ifError(error);
 		
-		s3.putLifeCycleRule('id2', 'otherprefix-' + timestamp, 5, function (error, response) {
-			callbacks.putLifeCycleRule2++;
+		s3.delLifeCycleRule('id', function(error, response) {
+			callbacks.delLifeCycleRule++;
 			
-			console.log('s3.putLifeCycleRule: id2, otherprefix-' + timestamp);
+			console.log('s3.delLifeCycleRule: id');
 			showError(error);
 			assert.ifError(error);
-			
-			setTimeout(function () {
-				s3.delLifeCycleRule('id', function(error, response) {
-					callbacks.delLifeCycleRule1++;
-					
-					console.log('s3.delLifeCycleRule: id');
-					showError(error);
-					assert.ifError(error);
-					
-					s3.delLifeCycleRule('id2', function(error, response) {
-						callbacks.delLifeCycleRule2++;
-						
-						console.log('s3.delLifeCycleRule: id2');
-						showError(error);
-						assert.ifError(error);
-					});
-				});
-			}, 18000);
 		});
 	});
 });
