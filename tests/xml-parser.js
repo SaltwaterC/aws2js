8f8f3e42dda919e2c4b292f71d1610571af9dc88
Ștefan Rusu <saltwaterc@gmail.com>
2013-01-24 15:38:42 +0200

    The v0.8 changes in install.

--- tests/xml-parser.js
+++ tests/xml-parser.js
@@ -3,35 +3,62 @@
 var common = require('./includes/common.js');
 
 var fs = require('fs');
+var util = require('util');
 var assert = require('assert');
 
 var config = require('../config/aws.js');
-var dependencies = require('../config/dependencies.js');
 
-var xmlDep = require(dependencies.xml);
-var mimeDep = require(dependencies.mime);
-
-var callbacks = {
-	parser: 0
+var have = {
+	'libxml-to-js': true,
+	'xml2js': true
 };
 
-var parser;
-if (dependencies.xml === 'libxml-to-js') {
-	parser = xmlDep;
-} else { // xml2js
-	parser = new xmlDep.Parser(config.xml2jsConfig).parseString;
+var callbacks = {
+	'libxml-to-js': 0,
+	'xml2js': 0
+};
+
+try {
+	require('libxml-to-js');
+} catch (e) {
+	have['libxml-to-js'] = false;
+	callbacks['libxml-to-js']++;
+	util.log('libxml-to-js is not installed, moving on without it');
 }
 
-fs.readFile('data/ec2-describeimages.xml', function (err, data) {
-	assert.ifError(err);
+try {
+	require('xml2js');
+} catch (e) {
+	util.error('ERROR: xml2js is missing, can not test the installation');
+	process.exit(1);
+}
+
+var testXml = function (lib) {
+	util.log('testing the XML library ' + lib);
 	
-	var xml = new Buffer(data).toString();
-	parser(xml, function (err, res) {
-		callbacks.parser++;
+	var parser = require(lib);
+	if (lib === 'xml2js') {
+		parser = new parser.Parser(config.xml2jsConfig).parseString;
+	}
+	
+	fs.readFile('data/ec2-describeimages.xml', function (err, data) {
 		assert.ifError(err);
-		assert.equal(res.imagesSet.item[0].imageId, 'ami-be3adfd7');
-		assert.equal(res.imagesSet.item[1].imageId, 'ami-be3adfd9');
+		
+		var xml = new Buffer(data).toString();
+		parser(xml, function (err, res) {
+			callbacks[lib]++;
+			assert.ifError(err);
+			assert.equal(res.imagesSet.item[0].imageId, 'ami-be3adfd7');
+			assert.equal(res.imagesSet.item[1].imageId, 'ami-be3adfd9');
+		});
 	});
-});
+};
+
+var idx;
+for (idx in have) {
+	if (have.hasOwnProperty(idx) && have[idx] === true) {
+		testXml(idx);
+	}
+}
 
 common.teardown(callbacks);
