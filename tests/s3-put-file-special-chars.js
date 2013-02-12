861eca7bfd2dac3f446957ee000ba19dd55b2e7d
Ștefan Rusu <saltwaterc@gmail.com>
2013-02-12 12:52:50 +0200

    Added Content-Type and Content-Lenght to S3 PUT foo.txt as regression for #78.

--- tests/s3-put-file-special-chars.js
+++ tests/s3-put-file-special-chars.js
@@ -15,7 +15,10 @@ var callbacks = {
 s3.setCredentials(process.env.AWS_ACCEESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
 s3.setBucket(process.env.AWS2JS_S3_BUCKET);
 
-s3.putFile(path, './data/foo.txt', false, {}, function (err, res) {
+s3.putFile(path, './data/foo.txt', false, {
+	'content-type': 'text/plain',
+	'content-length': 4
+}, function (err, res) {
 	callbacks.put++;
 	assert.ifError(err);
 	s3.get(path, 'buffer', function (err, res) {
